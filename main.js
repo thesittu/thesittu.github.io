094251d6661df639e22d76e98c6df86e214fff87
Ayushi Agrawal <stdayushi@gmail.com>
2020-03-10 18:18:18 +0530

    rotating hand added

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-console.log("Hello World");
+var timeout;
+$('#paraBack').mousemove(function (e) {
+    console.log("Mousse Over");
+    if (timeout) clearTimeout(timeout);
+    setTimeout(callParallax.bind(null, e), 200);
 
-let imgUrl= "https://source.unsplash.com/random";
-$("body").css('background-image', 'url(' + imgUrl + ')');
+});
+
+function callParallax(e) {
+    parallaxIt(e, '.paraSlide', -100);
+    parallaxIt(e, 'img', -30);
+}
+
+function parallaxIt(e, target, movement) {
+    var $this = $('#paraBack');
+    var relX = e.pageX - $this.offset().left;
+    var relY = e.pageY - $this.offset().top;
+
+    TweenMax.to(target, 1, {
+        x: (relX - $this.width() / 2) / $this.width() * movement,
+        y: (relY - $this.height() / 2) / $this.height() * movement,
+        ease: Power2.easeOut
+    })
+}
